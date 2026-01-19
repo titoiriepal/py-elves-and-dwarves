e99ffcec6104be6266098a848f16322fc43eeeb6
titoiriepal <titoiriepal@hotmail.com>
2026-01-19 13:13:43 +0100

    Abstract class Player, class Elf and class Dwarf created

--- app/main.py
+++ app/main.py
@@ -1,1 +1,37 @@
-# write your code here
+from abc import ABC, abstractmethod
+
+
+class Player(ABC):
+
+    def __init__(self, nickname: str) -> None:
+        self.nickname = nickname
+
+    @abstractmethod
+    def get_rating(self) -> None:
+        pass
+
+    @abstractmethod
+    def player_info(self) -> None:
+        pass
+
+
+class Elf(Player):
+
+    def __init__(self, nickname: str, musical_instrument: str) -> None:
+        super().__init__(nickname)
+        self._musical_instrument = musical_instrument
+
+    def play_elf_song(self) -> None:
+        text = f"{self.nickname} is playing"
+        text += f" a song on the {self._musical_instrument}"
+        print(text)
+
+
+class Dwarf(Player):
+
+    def __init__(self, nickname: str, favourite_dish: str) -> None:
+        super().__init__(nickname)
+        self._favourite_dish = favourite_dish
+
+    def eat_favourite_dish(self) -> None:
+        print(f"{self.nickname} is eating {self._favourite_dish}")
